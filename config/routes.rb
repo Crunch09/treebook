5ac7792058d395fef3745159a7b1ad4e2c940da4
--global <flo@florianthomas.net>
2012-02-14 09:57:25 +0100

    Root-Route changed to registration/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Treebook::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/', :to => "devise/sessions#new"
+  end
 
   resources :trees
 
@@ -9,7 +11,6 @@ Treebook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
