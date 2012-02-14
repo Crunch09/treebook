5e9d253d8992f547b2c29c45bb8fabdd105de45c
--global <flo@florianthomas.net>
2012-02-14 09:42:23 +0100

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Treebook::Application.routes.draw do
+  devise_for :users
+
   resources :trees
 
   resources :posts
 
   resources :users
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+   root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,9 +58,7 @@ Treebook::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
