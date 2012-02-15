64a321b14eac282d8a5ac3906686176416c73462
Christian Brandt <christianbrandt90@gmail.com>
2012-02-15 10:34:29 +0100

    intro music player

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -15,3 +15,25 @@ var show = function(str) {
     $('#navigation').find('a[name="'+str+'"]').addClass('active');
   }
 }
+
+var initMusicPlayer() {
+  var playlist = {
+		mp3:'/assets/media/intro_music.mp3',
+		oga:'/assets/media/intro_music.ogg',
+        rating:4.5,
+        title:'Threshold (8-bit)',
+        duration:'1:50',
+        artist:'Brian LeBarton'
+	};
+	
+	$('#player').ttwMusicPlayer(myPlaylist,
+	  {
+      tracksToShow:1,
+      autoPlay:true,
+      ratingCallback:function(index, playlistItem, rating){
+              //some logic to process the rating, perhaps through an ajax call
+      },
+      jPlayer:{} //override default jPlayer options here. This accepts the same structure as the standalone jPlayer
+	  }
+	);
+}
