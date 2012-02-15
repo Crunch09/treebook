b04362b108a884d9fb0fcff8ad74e6c275f16ebd
--global <flo@florianthomas.net>
2012-02-15 10:28:05 +0100

    Removed unnecessary function

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,4 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :name, :birthday, :trees, :posts
 
-
-	def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-		data = access_token.extra.raw_info
-	  	if user = User.where(:email => data.email).first
-	    	user
-	  	else # Create a user with a stub password. 
-	    	User.create!(:email => data.email, :password => Devise.friendly_token[0,20], :firstname => data.given_name, :name => data.family_name) 
-	  	end
-	end
 end
