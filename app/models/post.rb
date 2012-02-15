64652a8db4d80dbbf7555a2281de7e2f9efbcc4e
--global <flo@florianthomas.net>
2012-02-15 13:41:49 +0100

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :comment_to, :class_name => "Post",
     :foreign_key => "post_id"
   has_and_belongs_to_many :trees
+
+  attr_accessible :text, :likes, :dislikes, :user, :comment_to
+  
+  validates :text, :presence => true
+  validates :user, :presence => true
 end
