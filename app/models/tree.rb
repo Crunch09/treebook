3ef17c22b25d3016706d8c6795d94ec3e4c8e2a6
--global <flo@florianthomas.net>
2012-03-13 15:11:20 +0100

    fixes bug in adding trees

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -3,7 +3,7 @@ class Tree < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :posts
 
-  attr_accessible :title, :user
+  attr_accessible :title, :user, :user_id
 
   validates :title, :presence => true
   validates :user, :presence => true
