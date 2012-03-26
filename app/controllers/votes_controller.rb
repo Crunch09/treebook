470d0982ede040b4a31c27da92c62dae88e83fdc
--global <flo@florianthomas.net>
2012-03-26 12:44:29 +0200

    Fixed typo

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,16 +3,17 @@ class VotesController < ApplicationController
 		@vote = Vote.new(params[:vote])
 
 		respond_to do |format|
-        if @vote.save
-        	if @vote.upvote == true
-        		@vote.post.likes = @vote.post.likes + 1
-        	else
-        		@vote.post.dislikes = @vote.post.dislikes + 1
-        	end
-        	@vote.post.save
-        	format.json { render json: @vote, status: :created, location: @vote }
-      	else
-        	format.json { render json: @vote.errors, status: :unprocessable_entity }
-      	end
+	        if @vote.save
+	        	if @vote.upvote == true
+	        		@vote.post.likes = @vote.post.likes + 1
+	        	else
+	        		@vote.post.dislikes = @vote.post.dislikes + 1
+	        	end
+	        	@vote.post.save
+	        	format.json { render json: @vote, status: :created, location: @vote }
+	      	else
+	        	format.json { render json: @vote.errors, status: :unprocessable_entity }
+	      	end
+	    end
     end
 end
