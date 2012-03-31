a2a716be798c10e06c7baaf61fbde46983d7893f
--global <flo@florianthomas.net>
2012-03-31 15:30:13 +0200

    Added gravater link to users info

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -2,87 +2,99 @@ class TreesController < ApplicationController
 
   before_filter :authenticate_user!
 
-  # GET /trees/1
+  # Public: Gibt Infos zu einem Tree
+  #
+  # id - Die Id des Trees
+  #
+  # Beispiele:
+  #
   # GET /trees/1.json
+  #
+  # Gibt den Tree zurück, oder falls
+  # dieser nicht dem aktuellen User gehört, eine Fehlermeldung
   def show
     @tree = Tree.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
       if @tree.user == current_user
-        format.json { render json: @tree.to_json(:include => { :user => {:only => [:firstname, :name, :id, :email]}, :users => {:only => [:firstname, :name, :id, :email]}}) }
+        format.json { render json: @tree.to_json(:include => { :user => {:only => [:firstname, :name, :id, :email]}, :users => {:only => [:firstname, :name, :id, :email], :methods => :image}}) }
       else
         format.json { render json: "Sie koennen leider nicht auf diesen Tree zugreifen", status: :unprocessable_entity }
       end
     end
   end
 
-  # GET /trees/new
-  # GET /trees/new.json
-  def new
-    @tree = Tree.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @tree }
-    end
-  end
-
-  # GET /trees/1/edit
-  def edit
-    @tree = Tree.find(params[:id])
-  end
-
-  # POST /trees
-  # POST /trees.json
+  # Public: Erstellt einen neuen Tree
+  #
+  # Beispiele:
+  #
+  #  POST /trees.json
+  #
+  # Gibt den erstellten Tree oder eine Fehlermeldung zurück 
   def create
     @tree = Tree.new(params[:tree])
     @tree.title = ERB::Util.h(@tree.title)
     respond_to do |format|
       if @tree.save
-        format.html { redirect_to @tree, notice: 'Tree was successfully created.' }
         format.json { render json: @tree, status: :created, location: @tree }
       else
-        format.html { render action: "new" }
         format.json { render json: @tree.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /trees/1
-  # PUT /trees/1.json
+  # Public: Aktualisiert einen bestehenden Tree
+  #
+  # id - Id des Trees
+  #
+  # Beispiele:
+  #
+  #  PUT /trees/1.json
+  #
+  # Gibt eine HTTP No-Content Meldung oder eine Fehlermeldung zurück
   def update
     @tree = Tree.find(params[:id])
 
     respond_to do |format|
       if @tree.update_attributes(params[:tree])
-        format.html { redirect_to @tree, notice: 'Tree was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
         format.json { render json: @tree.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /trees/1
-  # DELETE /trees/1.json
+  # Public: Löscht einen Tree
+  #
+  # id - Id des Trees
+  #
+  # Beispiele:
+  #
+  #  DELETE /trees/1.json
+  #
+  # Gibt nichts zurück
   def destroy
     @tree = Tree.find(params[:id])
     @tree.destroy
 
     respond_to do |format|
-      format.html { redirect_to trees_url }
       format.json { head :no_content }
     end
   end
 
 
-  #GET /trees
-  #GET /trees.json
+  # Public: Listet alle Trees des angemeldeten Users auf
+  #
+  # Beispiele:
+  #
+  #  GET /trees.json
+  #
+  # Gibt die Trees des Users zurück
   def index 
       respond_to do |format|
-        format.json { render json: current_user.owned_trees.to_json(:include => { :users => {:only => [:firstname, :name, :id, :email]}})}
+        format.json { render json: current_user.owned_trees.to_json(
+          :include => { :users => {:only => [:firstname, :name, :id, :email]}}
+        )}
       end
   end
 
