bb19ecf551845f6d477b9bfcfb4aba68d895736c
Florian Thomas <flo@florianthomas.net>
2012-04-07 18:45:32 +0200

    Added some documentation for gems and backend

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -19,7 +19,7 @@ class TreesController < ApplicationController
       if @tree.user == current_user
         format.json { render json: @tree.to_json(:include => { :user => {:only => [:firstname, :name, :id, :email]}, :users => {:only => [:firstname, :name, :id, :email], :methods => :image}}) }
       else
-        format.json { render json: "Sie koennen leider nicht auf diesen Tree zugreifen", status: :unprocessable_entity }
+        format.json { render json: "Du kannst leider nicht auf diesen Tree zugreifen", status: :unprocessable_entity }
       end
     end
   end
